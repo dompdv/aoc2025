c2ad46d5834eba3ffe60a109104afc233ea823fb
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-02 00:02:22 +0100

    Day01 nicer

--- lib/advent_of_code/solution/year_2025/day_01.ex
+++ lib/advent_of_code/solution/year_2025/day_01.ex
@@ -1,33 +1,34 @@
 defmodule AdventOfCode.Solution.Year2025.Day01 do
-  def next_position(current, delta, multiple), do: rem(current + delta + multiple, multiple)
+  def remp(n, p) when rem(n, p) == 0, do: 0
+  def remp(n, p) when n > 0, do: rem(n, p)
+  def remp(n, p), do: rem(n, p) + p
 
-  def dial(input, start, multiple, count_hits) do
-    # Parse
+  def dial(input, start, mult, counter_func) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(fn
       "L" <> rest -> -1 * String.to_integer(rest)
       "R" <> rest -> String.to_integer(rest)
     end)
-    # Dial
     |> Enum.reduce(
       {start, 0},
-      fn delta, {current_position, current_score} ->
-        {next_position(current_position, delta, multiple),
-         current_score + count_hits.(current_position, delta, multiple)}
+      fn delta, {position, score} ->
+        {next_position(position, delta, mult), score + counter_func.(position, delta, mult)}
       end
     )
-    # Take the score
     |> elem(1)
   end
 
-  def count_hits(start, delta, multiple) do
+  def next_position(current, delta, mult), do: remp(current + delta, mult)
+
+  # Brute force, but simple
+  def count_hits(start, delta, mult) do
     step = if delta < 0, do: -1, else: 1
-    Enum.count((start + step)..(start + delta)//step, fn i -> rem(i, multiple) == 0 end)
+    Enum.count((start + step)..(start + delta)//step, fn i -> remp(i, mult) == 0 end)
   end
 
-  def stop_at_zero(start, delta, multiple) do
-    if next_position(start, delta, multiple) == 0, do: 1, else: 0
+  def stop_at_zero(start, delta, mult) do
+    if next_position(start, delta, mult) == 0, do: 1, else: 0
   end
 
   def part1(input), do: dial(input, 50, 100, &stop_at_zero/3)
