a98de22389ef70fb41da86509f87b844b64389e2
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-02 15:25:11 +0100

    Nicer

--- lib/advent_of_code/solution/year_2025/day_02.ex
+++ lib/advent_of_code/solution/year_2025/day_02.ex
@@ -14,33 +14,27 @@ defmodule AdventOfCode.Solution.Year2025.Day02 do
   def is_invalid(n, stop_at) do
     n_into_chars = n |> to_string() |> to_charlist()
     number_length = length(n_into_chars)
-    test_until = if stop_at == nil, do: number_length, else: stop_at
+    test_until = if stop_at == :number_length, do: number_length, else: stop_at
 
     # Cut into all sizes of pieces
-    all_divs =
-      for i <- 2..test_until do
-        if rem(number_length, i) != 0 do
-          # If the number can be cut in equal sizes
-          false
-        else
-          # Test if all chunks are equal
-          Enum.chunk_every(n_into_chars, div(number_length, i))
-          |> Enum.uniq()
-          |> then(&(length(&1) == 1))
-        end
+    for i <- 2..test_until do
+      if rem(number_length, i) != 0 do
+        # If the number can not be cut in equal sizes
+        false
+      else
+        # Test if all chunks are equal
+        Enum.chunk_every(n_into_chars, div(number_length, i))
+        |> Enum.uniq()
+        |> then(&(length(&1) == 1))
       end
-
-    Enum.any?(all_divs)
+    end
+    |> Enum.any?()
   end
 
   def scan(input, stop_at) do
-    Enum.sum(
-      for {l, h} <- parse(input), n <- l..h, is_invalid(n, stop_at) do
-        n
-      end
-    )
+    Enum.sum(for {l, h} <- parse(input), n <- l..h, is_invalid(n, stop_at), do: n)
   end
 
   def part1(input), do: scan(input, 2)
-  def part2(input), do: scan(input, nil)
+  def part2(input), do: scan(input, :number_length)
 end
