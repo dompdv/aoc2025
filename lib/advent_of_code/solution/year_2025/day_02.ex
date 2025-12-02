5686d14608d982ddca9b6ef70975818e6ad25228
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-02 22:08:52 +0100

    A little bit more readable

--- lib/advent_of_code/solution/year_2025/day_02.ex
+++ lib/advent_of_code/solution/year_2025/day_02.ex
@@ -1,38 +1,37 @@
 defmodule AdventOfCode.Solution.Year2025.Day02 do
+  import Enum, only: [map: 2, chunk_every: 2, any?: 1, sum: 1]
+
   def parse(input) do
     input
     |> String.trim()
     |> String.split(",", trim: true)
-    |> Enum.map(fn r ->
+    |> map(fn r ->
       [l, h] = String.split(r, "-")
       {String.to_integer(l), String.to_integer(h)}
     end)
   end
 
-  def is_invalid(n, _stop_at) when n < 10, do: false
+  # Alternative: def uniform_list?(l), do: length(Enum.uniq(l)) == 1
+  def uniform_list?([_a]), do: true
+  def uniform_list?([a, b | _r]) when a != b, do: false
+  def uniform_list?([_a, b | r]), do: uniform_list?([b | r])
+
+  def is_invalid(n, _stop_at) when n <= 9, do: false
 
   def is_invalid(n, stop_at) do
     n_into_chars = n |> to_string() |> to_charlist()
     number_length = length(n_into_chars)
     test_until = if stop_at == :number_length, do: number_length, else: stop_at
 
-    # Cut into all sizes of pieces
-    for i <- 2..test_until do
-      if rem(number_length, i) != 0 do
-        # If the number can not be cut in equal sizes
-        false
-      else
-        # Test if all chunks are equal
-        Enum.chunk_every(n_into_chars, div(number_length, i))
-        |> Enum.uniq()
-        |> then(&(length(&1) == 1))
-      end
+    # Cut into all sizes of equal pieces (length must be divisible into equal size)
+    for i <- 2..test_until, rem(number_length, i) == 0 do
+      n_into_chars |> chunk_every(div(number_length, i)) |> uniform_list?()
     end
-    |> Enum.any?()
+    |> any?()
   end
 
   def scan(input, stop_at) do
-    Enum.sum(for {l, h} <- parse(input), n <- l..h, is_invalid(n, stop_at), do: n)
+    sum(for {l, h} <- parse(input), n <- l..h, is_invalid(n, stop_at), do: n)
   end
 
   def part1(input), do: scan(input, 2)
