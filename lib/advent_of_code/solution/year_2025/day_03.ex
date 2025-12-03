9393304ae8881deda15b32ad8869d834b4fd3d0a
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-03 21:16:21 +0100

    with comments

--- lib/advent_of_code/solution/year_2025/day_03.ex
+++ lib/advent_of_code/solution/year_2025/day_03.ex
@@ -16,11 +16,15 @@ defmodule AdventOfCode.Solution.Year2025.Day03 do
   def part1(input), do: parse(input) |> Enum.map(&largest_two/1) |> Enum.sum()
 
   ### Part 2
+  # End of recursion: all switches are activated
   def search(_, cumul, -1), do: cumul
 
   def search(rest, cumul, n_switches) do
+    # Consider the max number on the right (keeping enough numbers for all the switches that are still to activate)
     max_in_remaining = rest |> Enum.slice(0..(length(rest) - n_switches - 1)) |> Enum.max()
+    # Drop the numbers to the right until (and including) this max
     {_, [_max_in_remaining | r]} = Enum.split_while(rest, &(&1 != max_in_remaining))
+    # Loop recursively
     search(r, cumul + max_in_remaining * Integer.pow(10, n_switches), n_switches - 1)
   end
 
