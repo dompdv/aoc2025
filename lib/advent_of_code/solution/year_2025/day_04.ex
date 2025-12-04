8df4dd24e9bcd880a1298f6116bf54cc60e4aab8
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-04 19:10:01 +0100

    Cleaner

--- lib/advent_of_code/solution/year_2025/day_04.ex
+++ lib/advent_of_code/solution/year_2025/day_04.ex
@@ -1,40 +1,35 @@
 defmodule AdventOfCode.Solution.Year2025.Day04 do
+  import Enum, only: [flat_map: 2, with_index: 1, sum: 1, filter: 2, count: 1]
+
   def parse(input) do
     # Create a MapSet of the {row,col} of the rolls
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.with_index()
-    |> Enum.flat_map(fn {line, row} ->
-      line
-      |> to_charlist()
-      |> Enum.with_index()
-      |> Enum.reduce([], fn
-        {?., _}, acc -> acc
-        {?@, col}, acc -> [{row, col} | acc]
-      end)
+    with_index(String.split(input, "\n", trim: true))
+    |> flat_map(fn {line, row} ->
+      for {c, col} <- with_index(to_charlist(line)), c == ?@, do: {row, col}
     end)
     |> MapSet.new()
   end
 
-  @around for r <- -1..1, c <- -1..1, {r, c} != {0, 0}, do: {r, c}
+  def vec_sum({a, b}, {c, d}), do: {a + c, b + d}
 
-  def liftable({r_roll, c_roll}, rolls) do
-    Enum.sum(for {r, c} <- @around, {r_roll + r, c_roll + c} in rolls, do: 1) < 4
+  @around for r <- -1..1, c <- -1..1, {r, c} != {0, 0}, do: {r, c}
+  def liftable(roll, rolls) do
+    sum(for delta <- @around, vec_sum(roll, delta) in rolls, do: 1) < 4
   end
 
   def part1(input) do
     rolls = parse(input)
-    Enum.filter(rolls, &liftable(&1, rolls)) |> Enum.count()
+    rolls |> filter(&liftable(&1, rolls)) |> count()
   end
 
-  def remove_process(rolls, counter) do
+  def remove_all(rolls, counter \\ 0) do
     # Identify the removable rolls
     removable = for roll <- rolls, liftable(roll, rolls), into: MapSet.new(), do: roll
     # If nothing to remove, we're done, else remove the removable rolls and increment the counter
     if MapSet.size(removable) == 0,
       do: counter,
-      else: remove_process(MapSet.difference(rolls, removable), counter + MapSet.size(removable))
+      else: remove_all(MapSet.difference(rolls, removable), counter + MapSet.size(removable))
   end
 
-  def part2(input), do: remove_process(parse(input), 0)
+  def part2(input), do: input |> parse() |> remove_all()
 end
