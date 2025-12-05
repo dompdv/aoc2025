3cfddee5a865508b38b8ca842b73f6d620d8a495
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-05 19:34:00 +0100

    Optimal

--- lib/advent_of_code/solution/year_2025/day_05.ex
+++ lib/advent_of_code/solution/year_2025/day_05.ex
@@ -1,13 +1,14 @@
 defmodule AdventOfCode.Solution.Year2025.Day05 do
+  import Enum, only: [map: 2, any?: 1, count: 2, sort: 1, sum: 1]
   # Part 1
-  def in_interval?(i, {l, h}), do: l <= i and i <= h
+  def in_range?(i, {l, h}), do: l <= i and i <= h
 
-  def in_any_interval?(ingredient, intervals),
-    do: Enum.any?(for interval <- intervals, do: in_interval?(ingredient, interval))
+  def in_any_range?(ingredient, ranges),
+    do: any?(for range <- ranges, do: in_range?(ingredient, range))
 
   def part1(input) do
-    {intervals, ingredients} = parse(input)
-    Enum.count(ingredients, &in_any_interval?(&1, intervals))
+    {ranges, ingredients} = parse(input)
+    count(ingredients, &in_any_range?(&1, ranges))
   end
 
   # Part 2
@@ -15,42 +16,30 @@ defmodule AdventOfCode.Solution.Year2025.Day05 do
   def merge_overlapping({a, b}, {c, d}) when c < a, do: merge_overlapping({c, d}, {a, b})
   def merge_overlapping({a, b}, {_c, d}), do: {a, max(b, d)}
 
-  def merge([], intervals_in_stock), do: intervals_in_stock
+  def merge([], ranges_in_stock), do: ranges_in_stock
+  def merge([range | to_merge], []), do: merge(to_merge, [range])
 
-  def merge([to_add | to_merge], intervals_in_stock) do
-    case Enum.split_while(intervals_in_stock, &distinct?(&1, to_add)) do
-      {_, []} ->
-        merge(to_merge, [to_add | intervals_in_stock])
-
-      {distincts, [overlapping | in_stock]} ->
-        merged_interval = merge_overlapping(to_add, overlapping)
-        merge([merged_interval | to_merge], distincts ++ in_stock)
-    end
-  end
-
-  def add_interval_sizes(intervals) do
-    intervals |> Enum.map(fn {l, h} -> h - l + 1 end) |> Enum.sum()
+  def merge([to_add | to_merge], [top_of_stock | rest] = stock) do
+    if distinct?(to_add, top_of_stock),
+      do: merge(to_merge, [to_add | stock]),
+      else: merge(to_merge, [merge_overlapping(to_add, top_of_stock) | rest])
   end
 
   def part2(input) do
     elem(parse(input), 0)
-    |> Enum.sort_by(& &1)
+    |> sort()
     |> merge([])
-    |> add_interval_sizes()
+    |> map(fn {l, h} -> h - l + 1 end)
+    |> sum()
   end
 
   def parse(input) do
-    [intervals, ingredients] = String.split(input, "\n\n", trim: true)
+    [p1, p2] = String.split(input, "\n\n") |> map(&String.split(&1, "\n", trim: true))
 
-    p_intervals =
-      for int <- String.split(intervals, "\n") do
-        [l, h] = String.split(int, "-")
-        {String.to_integer(l), String.to_integer(h)}
-      end
+    {map(p1, &parse_range/1), map(p2, &String.to_integer/1)}
+  end
 
-    p_ingredients =
-      for ing <- String.split(ingredients, "\n", trim: true), do: String.to_integer(ing)
-
-    {p_intervals, p_ingredients}
+  def parse_range(line) do
+    line |> String.split("-") |> map(&String.to_integer/1) |> List.to_tuple()
   end
 end
