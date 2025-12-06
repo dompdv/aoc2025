d8eee452da92a2e68bda13e98e41549b93db9a85
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-06 17:03:54 +0100

    Day6, far cleaner

--- lib/advent_of_code/solution/year_2025/day_06.ex
+++ lib/advent_of_code/solution/year_2025/day_06.ex
@@ -1,12 +1,21 @@
 defmodule AdventOfCode.Solution.Year2025.Day06 do
   import Enum, only: [with_index: 1, reduce: 3, sum: 1, take: 2]
+  @space 32
 
-  def mul(l), do: reduce(l, 1, &(&1 * &2))
+  def part1(input), do: sum_mul(input, &parse_part1/1)
+  def part2(input), do: sum_mul(input, &parse_part2/1)
+
+  def sum_mul(input, parsing_function) do
+    sum(for {op, l} <- parsing_function.(input), do: sum_or_mul(op, l))
+  end
+
+  def sum_or_mul(:mul, l), do: mul(l)
+  def sum_or_mul(:add, l), do: sum(l)
 
   def parse_part1(input) do
     # Returns a list of groups like {:mul, [12,1453,23]} (operator, operands)
-    input
-    |> String.split("\n", trim: true)
+
+    String.split(input, "\n", trim: true)
     |> reduce(
       %{},
       fn line, acc ->
@@ -31,58 +40,43 @@ defmodule AdventOfCode.Solution.Year2025.Day06 do
     |> Map.values()
   end
 
-  def part1(input) do
-    parse_part1(input)
-    |> reduce(0, fn
-      {op, l}, acc ->
-        acc + if op == :mul, do: mul(l), else: sum(l)
-    end)
-  end
-
-  def parse2(input) do
+  def parse_part2(input) do
+    # Returns a list of groups like {:mul, [12,1453,23]} (operator, operands)
     lines = input |> String.split("\n", trim: true)
-
-    {
-      # map of %{column => number}
+    # Build map of %{column => number in the column}
+    numbers =
       reduce(take(lines, length(lines) - 1), %{}, fn line, acc ->
         line
         |> String.codepoints()
         |> with_index()
         |> reduce(acc, fn {c, col}, local_acc ->
-          Map.update(local_acc, col, c, &(&1 <> c))
+          Map.update(local_acc, col, c, &String.trim(&1 <> c))
         end)
       end)
       |> reduce(%{}, fn {col, s}, acc ->
-        s = String.trim(s)
         if s == "", do: acc, else: Map.put(acc, col, String.to_integer(s))
       end)
-      |> Map.new(),
-      # map of %{column => operator}
-      List.last(lines)
-      |> to_charlist()
-      |> with_index()
-      |> reduce(%{}, fn {c, col}, acc ->
-        if c == 32, do: acc, else: Map.put(acc, col, c)
-      end)
-    }
+      |> Map.new()
+
+    # Build the [{operator, list of numbers}]
+    List.last(lines)
+    |> to_charlist()
+    |> with_index()
+    |> reduce(%{}, fn
+      {@space, _col}, acc ->
+        acc
+
+      {op, col}, acc ->
+        atom_op = if op == ?*, do: :mul, else: :add
+        Map.put(acc, col, {atom_op, gather(numbers, col, [])})
+    end)
+    |> Map.values()
   end
 
-  # Find the next element in a list. returns max_length if reaching the end of the list
-  def next([val], val, max_length), do: max_length
-  def next([val, n | _], val, _max_length), do: n
-  def next([_n | r], val, max_length), do: next(r, val, max_length)
+  def mul(l), do: reduce(l, 1, &(&1 * &2))
 
   # Gather numbers starting from col till an empty column
   def gather(numbers, col, acc) do
     if Map.has_key?(numbers, col), do: gather(numbers, col + 1, [numbers[col] | acc]), else: acc
-  end
-
-  def part2(input) do
-    {numbers, operators} = parse2(input)
-
-    reduce(operators, 0, fn {col, op}, acc ->
-      l = gather(numbers, col, [])
-      acc + if op == ?*, do: mul(l), else: sum(l)
-    end)
   end
 end
