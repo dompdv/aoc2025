33d3377f0f942a0b23295ec2d859e818b683745c
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-07 09:55:34 +0100

    Day7 nicer

--- lib/advent_of_code/solution/year_2025/day_07.ex
+++ lib/advent_of_code/solution/year_2025/day_07.ex
@@ -1,52 +1,39 @@
 defmodule AdventOfCode.Solution.Year2025.Day07 do
   def part1(input), do: input |> fire_beam() |> take_counter()
   def part2(input), do: input |> fire_beam() |> take_timelines() |> Map.values() |> Enum.sum()
-
-  def fire_beam(input) do
-    layout = parse(input)
-    last_row = layout |> Map.keys() |> Enum.max()
-    [start] = layout[0]
-    beam(Map.new([{start, 1}]), 0, last_row, 0, layout)
-  end
-
   def take_counter(a), do: elem(a, 1)
   def take_timelines(a), do: elem(a, 0)
 
-  def beam(in_flight, row, last_row, counter, _layout) when row > last_row,
-    do: {in_flight, counter}
+  def fire_beam(input) do
+    [[start] | rows] = parse(input)
+    beam(Map.new([{start, 1}]), 0, rows)
+  end
 
-  def beam(in_flight, row, last_row, counter, layout) do
+  def beam(in_flight, hits_counter, []), do: {in_flight, hits_counter}
+
+  def beam(in_flight, hits_counter, [next_row_splitters | rows]) do
     # in_flight %{column => number of timelines leading to this point}
-    # counter : number of hits on a splitter
-    next_row_splitters = Map.get(layout, row + 2, [])
+    # hits_counter : number of hits on a splitter
+    {new_in_flight, new_hits} =
+      Enum.reduce(in_flight, {%{}, 0}, fn {col, n_timelines}, {next_positions, hits} ->
+        if col in next_row_splitters,
+          do: {
+            next_positions
+            |> Map.update(col - 1, n_timelines, &(&1 + n_timelines))
+            |> Map.update(col + 1, n_timelines, &(&1 + n_timelines)),
+            hits + 1
+          },
+          else: {Map.update(next_positions, col, n_timelines, &(&1 + n_timelines)), hits}
+      end)
 
-    {new_in_flight, bings} =
-      Enum.reduce(
-        in_flight,
-        {%{}, 0},
-        fn {col, n_timelines}, {next_positions, bings_so_far} ->
-          if col in next_row_splitters do
-            {
-              next_positions
-              |> Map.update(col - 1, n_timelines, &(&1 + n_timelines))
-              |> Map.update(col + 1, n_timelines, &(&1 + n_timelines)),
-              bings_so_far + 1
-            }
-          else
-            {Map.update(next_positions, col, n_timelines, &(&1 + n_timelines)), bings_so_far}
-          end
-        end
-      )
-
-    beam(new_in_flight, row + 2, last_row, counter + bings, layout)
+    beam(new_in_flight, hits_counter + new_hits, rows)
   end
 
   def parse(input) do
     # Returns %{row => [list of splitter's columns]}
     input
     |> String.split("\n", trim: true)
-    |> Enum.with_index()
-    |> Enum.reduce([], fn {line, row}, lines ->
+    |> Enum.reduce([], fn line, lines ->
       splitter_cols =
         to_charlist(line)
         |> Enum.with_index()
@@ -56,8 +43,8 @@ defmodule AdventOfCode.Solution.Year2025.Day07 do
           {?S, col}, l -> [col | l]
         end)
 
-      if splitter_cols == [], do: lines, else: [{row, splitter_cols} | lines]
+      if splitter_cols == [], do: lines, else: [splitter_cols | lines]
     end)
-    |> Map.new()
+    |> Enum.reverse()
   end
 end
