7e21d34247f778ffc3108f6fff6576b0156a68de
dompdv <1734209+dompdv@users.noreply.github.com.>
2025-12-13 14:39:18 +0100

    Day10 p1

--- lib/advent_of_code/solution/year_2025/day_09.ex
+++ lib/advent_of_code/solution/year_2025/day_09.ex
@@ -1,7 +1,36 @@
 defmodule AdventOfCode.Solution.Year2025.Day09 do
-  def part1(_input) do
+  @test """
+  7,1
+  11,1
+  11,7
+  9,7
+  9,5
+  2,5
+  2,3
+  7,3
+  """
+  def part1(input) do
+    tiles = input |> parse()
+
+    for {i1, [x1, y1]} <- tiles,
+        {i2, [x2, y2]} <- tiles,
+        i1 < i2 do
+      (max(x1, x2) - min(x1, x2) + 1) * (max(y1, y2) - min(y1, y2) + 1)
+    end
+    |> Enum.max()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    points = parse(input)
+  end
+
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line |> String.split(",", trim: true) |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.with_index(&{&2, &1})
+    |> Map.new()
   end
 end
--- lib/advent_of_code/solution/year_2025/day_10.ex
+++ lib/advent_of_code/solution/year_2025/day_10.ex
@@ -1,7 +1,76 @@
 defmodule AdventOfCode.Solution.Year2025.Day10 do
-  def part1(_input) do
+  @test """
+  [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+  [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+  [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+  """
+
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.map(fn %{diagram: {target, _}} = machine ->
+      Graph.new()
+      |> Graph.add_edges(generate_edges(machine))
+      |> Graph.dijkstra(0, target)
+      |> Enum.count()
+      |> then(&(&1 - 1))
+    end)
+    |> Enum.sum()
   end
 
   def part2(_input) do
+  end
+
+  def generate_edges(%{diagram: {_, size}, buttons: buttons}) do
+    Enum.flat_map(0..(size - 1), fn start ->
+      for button <- buttons, do: {start, Bitwise.bxor(start, button), weight: 1}
+    end)
+  end
+
+  def parse(input) do
+    input |> String.split("\n", trim: true) |> Enum.map(&parse_line/1)
+  end
+
+  def parse_line(line) do
+    line
+    |> String.split(" ", trim: true)
+    |> Enum.reduce(%{diagram: nil, buttons: [], joltage: []}, fn chunk, acc ->
+      cond do
+        Regex.match?(~r/\[(.*)\]/, chunk) ->
+          %{acc | diagram: {parse_diagram(chunk), Integer.pow(2, String.length(chunk) - 2)}}
+
+        Regex.match?(~r/\{(.*)\}/, chunk) ->
+          %{acc | joltage: parse_joltage(chunk)}
+
+        true ->
+          Map.update(acc, :buttons, [], fn buttons -> [parse_button(chunk) | buttons] end)
+      end
+    end)
+  end
+
+  def parse_diagram(chunk) do
+    Regex.run(~r/\[(.*)\]/, chunk, capture: :all_but_first)
+    |> hd
+    |> to_charlist()
+    |> Enum.reduce({0, 1}, fn
+      ?., {acc, n} -> {acc, n * 2}
+      ?#, {acc, n} -> {acc + n, n * 2}
+    end)
+    |> elem(0)
+  end
+
+  def parse_button(button) do
+    Regex.run(~r/\((.*)\)/, button, capture: :all_but_first)
+    |> hd()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(0, fn n, acc -> acc + Integer.pow(2, n) end)
+  end
+
+  def parse_joltage(chunk) do
+    Regex.run(~r/\{(.*)\}/, chunk, capture: :all_but_first)
+    |> hd
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 end
